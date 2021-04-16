971e3159dd42661d04e910fea0e4fdd642cbd596
Kamil Dabrowski <kamild973@gmail.com>
2021-04-16 20:39:34 +0200

    Clean up files

---
+++ web_app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/')
+def hello_world():
+	return {"message": "Hello world"}
+
+
+class HelloNameResp(BaseModel):
+	message: str
+
+
+@app.get('/hello/{name}', response_model = HelloNameResp)
+def hello_name(name: str):
+	return HelloNameResp(message=f"Hello {name}")
